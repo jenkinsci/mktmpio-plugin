1a283205d7f17dd828d1c00997a211029f1be718
Ryan Graham <r.m.graham@gmail.com>
2015-08-04 11:29:52 -0700

    test: MktmpioClient tests for offline failures

--- src/test/java/org/jenkinsci/plugins/mktmpio/MktmpioClientTest.java
+++ src/test/java/org/jenkinsci/plugins/mktmpio/MktmpioClientTest.java
@@ -8,6 +8,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.not;
 
 public class MktmpioClientTest extends MktmpioBaseTest {
     @Test
@@ -30,5 +31,29 @@ public class MktmpioClientTest extends MktmpioBaseTest {
         assertThat(redis.getUsername(), isEmptyString());
         assertThat(redis.getType(), is("redis"));
         client.destroy(redis);
+    }
+
+    @Test
+    public void testOfflineCreate() throws Exception {
+        MktmpioClient client = new MktmpioClient("http://127.0.0.1:1", "does-not-matter");
+        try {
+            client.create("redis");
+            org.junit.Assert.fail("should client.create should have thrown");
+        } catch (IOException ex) {
+            assertThat(ex.getMessage(), not(containsString("Authentication required")));
+        }
+
+    }
+
+    @Test
+    public void testOfflineDestroy() throws Exception {
+        final MktmpioClient client = new MktmpioClient("http://127.0.0.1:1", "does-not-matter");
+        final MktmpioInstance instance = new MktmpioInstance("", "", 0, "", "", "", "");
+        try {
+            client.destroy(instance);
+            org.junit.Assert.fail("should client.create should have thrown");
+        } catch (IOException ex) {
+            assertThat(ex.getMessage(), not(containsString("Authentication required")));
+        }
     }
 }
